5c9ae81fbec8ac3081cf7b3b7164eeea285d757a
EdwarsBosarreyes <bosaed07@gmail.com>
2021-08-05 13:18:02 -0600

    First Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default {
+  products: [
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.png',
+      price: 60,
+      brand: 'Nike',
+      rating: 5.5,
+      numReviews: 11
+    },
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.png',
+      price: 40,
+      brand: 'Nike',
+      rating: 4.3,
+      numReviews: 12
+    },
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.png',
+      price: 50,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.png',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    }
+  ]
+}
